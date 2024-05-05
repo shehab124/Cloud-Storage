eaa53150b66b7825c13c9234d6d5ae8ee06f99e7
chehab <chehab.gamal@gmail.com>
2024-05-05 17:24:32 +0300

    main config

---
+++ src/test/java/com/example/Cloud/Storage/CloudStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Cloud.Storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
